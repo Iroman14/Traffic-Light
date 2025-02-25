6b71994aea06b9d0ce23d6e114574f214fa49a58
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2025-02-25 07:15:04 +0000

    Proyecto

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+	const [selectedColor, setSelectedColor] = useState("red");
+	const [color, setColor] = useState(["red", "yellow", "green"]);
+	const [size, setSize] = useState("normal");
 
-//create your first component
-const Home = () => {
+	const ChangeColor = () => {
+		let color = ["red", "yellow", "green", "purple"];
+		let index = Math.floor(Math.random() * color.length);
+		setSelectedColor(color[index]);
+		console.log(color[index]);
+	}
+
+	const AddColor = () => {
+		if (!color.includes("purple")) {
+			setColor([...color, "purple"]);
+			setSize("Plus")
+			console.log(setSize);
+		}
+	};
+
+	const RemoveColor = () => {
+		setColor(color.filter(color => color !== "purple"));
+		setSize("normal");
+	};
+
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Container">
+			<div className="Cable"></div>
+			<div className={`Traffic-Light${size === "Plus" ? "-Plus" : ""}`}>
+				<div onClick={() => setSelectedColor("red")} className={"Light-Red" + (selectedColor === "red" ? " Shine" : "")}></div>
+				<div onClick={() => setSelectedColor("yellow")} className={"Light-Yellow" + (selectedColor === "yellow" ? " Shine" : "")}></div>
+				<div onClick={() => setSelectedColor("green")} className={"Light-Green" + (selectedColor === "green" ? " Shine" : "")}></div>
+				{color.includes("purple") && (
+					<div onClick={() => setSelectedColor("purple")} className={"Light-Purple" + (selectedColor === "purple" ? " Shine" : "")}></div>
+				)}
+			</div>
+			<div className="d-flex justify-content-center">
+				<button onClick={ChangeColor} className="btn btn-dark"> Cambiar Color </button>
+				<button onClick={AddColor} className="btn btn-dark"> Añadir Color Púrpura </button>
+				<button onClick={RemoveColor} className="btn btn-dark"> Quitar Color Púrpura </button>
+			</div>
 		</div>
 	);
 };
